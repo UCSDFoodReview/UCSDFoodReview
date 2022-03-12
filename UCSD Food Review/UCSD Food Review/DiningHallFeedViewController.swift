b47abb1f2bb8c3c845b3e6d84a1640dc6172a840
David Em-Yinn <demyinn@ucsd.edu>
2022-03-12 15:12:03 -0800

    added constraints

---
+++ UCSD Food Review/UCSD Food Review/DiningHallFeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiningHallFeedViewController.swift
+//  UCSD Food Review
+//
+//  Created by David Em-Yinn on 3/12/22.
+//
+
+import UIKit
+
+class DiningHallFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
